ce331c45d3ac18e543bb7770e295888993e8921a
paulmorello <paulmorello1@gmail.com>
2016-10-12 08:52:20 +1100

    Updated background and addition of user js file

---
+++ scripts/user_interaction.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var changeValueX = function() {
+//   if (playerOne === 'X') {
+//     document.getElementById('a').innerHTML = 'X';
+//   }
+// }
+//
+// var changeValueO = function() {
+//   if (playerTwo === 'O') {
+//     document.getElementById('c').innerHTML = 'O';
+//   }
+// }
+//
+// var changeA = document.getElementById('a');
+// changeA.addEventListener('click', changeValueX);
+
+//
+// var changeC = document.getElementById('c');
+// changeC.addEventListener('click', changeValueO);
+//
+// var changeE = document.getElementById('e');
+// changeE.addEventListener('click', changeValueX);
