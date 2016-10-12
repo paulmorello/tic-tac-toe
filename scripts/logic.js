019616321b668123760e13b2848ac45c0c72bfab
paulmorello <paulmorello1@gmail.com>
2016-10-12 18:21:38 +1100

    Updated styles and condensed game logic Jquery

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,81 +1,74 @@
+// find squares on the board
+var square = $('.square');
 
-var squareOne = document.getElementById('a');
-var squareTwo = document.getElementById('b');
-var squareThree = document.getElementById('c');
-var squareFour = document.getElementById('d');
-var squareFive = document.getElementById('e');
-var squareSix = document.getElementById('f');
-var squareSeven = document.getElementById('g');
-var squareEight = document.getElementById('h');
-var squareNine = document.getElementById('i');
-
+// player total scores updated
+var playerOneTotal = 0;
+var playerTwoTotal = 0;
+var tieTotal = 0;
 
 
 var rowWinX = function() {
 
-  // if three row X win
+  //if three row X win
 
-  if (squareOne.innerHTML === 'x' && squareTwo.innerHTML === 'x' && squareThree.innerHTML === 'x') {
-    return 'win';
+  for (var i = 0; i < square.length; i++) {
+    if (square[0].innerHTML === 'x' && square[1].innerHTML === 'x' && square[2].innerHTML === 'x') {
+      return 'win';
+    }
+    else if (square[3].innerHTML === 'x' && square[4].innerHTML === 'x' && square[5].innerHTML === 'x') {
+      return 'win';
+    }
+    else if (square[6].innerHTML === 'x' && square[7].innerHTML === 'x' & square[8].innerHTML === 'x') {
+      return 'win';
+    }
   }
-
-  else if (squareFour.innerHTML === 'x' && squareFive.innerHTML === 'x' && squareSix.innerHTML === 'x') {
-    return 'win';
-  }
-
-  else if (squareSeven.innerHTML === 'x' && squareEight.innerHTML === 'x' && squareNine.innerHTML === 'x') {
-    return 'win';
-  }
-
 }
 
 var columnWinX = function() {
 
   // if three column X win
 
-  if (squareOne.innerHTML === 'x' && squareFour.innerHTML === 'x' && squareSeven.innerHTML === 'x') {
-    return 'win';
+  for (var i = 0; i < square.length; i++) {
+    if (square[0].innerHTML === 'x' && square[3].innerHTML === 'x' && square[6].innerHTML === 'x') {
+      return 'win';
+    }
+    else if (square[1].innerHTML === 'x' && square[4].innerHTML === 'x' && square[7].innerHTML === 'x') {
+      return 'win';
+    }
+    else if (square[3].innerHTML === 'x' && square[5].innerHTML === 'x' && square[8].innerHTML === 'x') {
+      return 'win';
+    }
   }
-
-  else if (squareTwo.innerHTML === 'x' && squareFive.innerHTML === 'x' && squareEight.innerHTML === 'x') {
-    return 'win';
-  }
-
-  else if (squareThree.innerHTML === 'x' && squareSix.innerHTML === 'x' && squareNine.innerHTML === 'x') {
-    return 'win';
-  }
-
 }
 
 var diagonalWinX = function() {
 
   // if three X diagonals win
 
-  if (squareOne.innerHTML === 'x' && squareFive.innerHTML === 'x' && squareNine.innerHTML === 'x') {
-    return 'win';
+  for (var i = 0; i < square.length; i++) {
+    if (square[0].innerHTML === 'x' && square[4].innerHTML === 'x' && square[8].innerHTML === 'x') {
+      return 'win';
+    }
+    else if (square[2].innerHTML === 'x' && square[4].innerHTML === 'x' && square[6].innerHTML === 'x') {
+      return 'win';
+    }
   }
-
-  else if (squareThree.innerHTML === 'x' && squareFive.innerHTML === 'x' && squareSeven.innerHTML === 'x') {
-    return 'win';
-  }
-
-
 }
 
 var rowWinO = function() {
 
   // if three row O win
 
-  if (squareOne.innerHTML === 'o' && squareTwo.innerHTML === 'o' && squareThree.innerHTML === 'o') {
-    return 'win';
-  }
-
-  else if (squareFour.innerHTML === 'o' && squareFive.innerHTML === 'o' && squareSix.innerHTML === 'o') {
-    return 'win';
-  }
-
-  else if (squareSeven.innerHTML === 'o' && squareEight.innerHTML === 'o' && squareNine.innerHTML === 'o') {
-    return 'win';
+  for (var i = 0; i < square.length; i++) {
+    if (square[0].innerHTML === 'o' && square[1].innerHTML === 'o' && square[2].innerHTML === 'o') {
+      return 'win';
+    }
+    else if (square[3].innerHTML === 'o' && square[4].innerHTML === 'o' && square[5].innerHTML === 'o') {
+      return 'win';
+    }
+    else if (square[6].innerHTML === 'o' && square[7].innerHTML === 'o' & square[8].innerHTML === 'o') {
+      return 'win';
+    }
   }
 }
 
@@ -83,60 +76,31 @@ var columnWinO = function() {
 
   // if three column O win
 
-  if (squareOne.innerHTML === 'o' && squareFour.innerHTML === 'o' && squareSeven.innerHTML === 'o') {
-    return 'win';
+  for (var i = 0; i < square.length; i++) {
+    if (square[0].innerHTML === 'o' && square[3].innerHTML === 'o' && square[6].innerHTML === 'o') {
+      return 'win';
+    }
+    else if (square[1].innerHTML === 'o' && square[4].innerHTML === 'o' && square[7].innerHTML === 'o') {
+      return 'win';
+    }
+    else if (square[3].innerHTML === 'o' && square[5].innerHTML === 'o' && square[8].innerHTML === 'o') {
+      return 'win';
+    }
   }
-
-  else if (squareTwo.innerHTML === 'o' && squareFive.innerHTML === 'o' && squareEight.innerHTML === 'o') {
-    return 'win';
-  }
-
-  else if (squareThree.innerHTML === 'o' && squareSix.innerHTML === 'o' && squareNine.innerHTML === 'o') {
-    return 'win';
-  }
-
 }
 
 var diagonalWinO = function() {
 
   // if three O diagonals win
 
-  if (squareOne.innerHTML === 'o' && squareFive.innerHTML === 'o' && squareNine.innerHTML === 'o') {
-    return 'win';
+  for (var i = 0; i < square.length; i++) {
+    if (square[0].innerHTML === 'o' && square[4].innerHTML === 'o' && square[8].innerHTML === 'o') {
+      return 'win';
+    }
+    else if (square[2].innerHTML === 'o' && square[4].innerHTML === 'o' && square[6].innerHTML === 'o') {
+      return 'win';
+    }
   }
-
-  else if (squareThree.innerHTML === 'o' && squareFive.innerHTML === 'o' && squareSeven.innerHTML === 'o') {
-    return 'win';
-  }
-
-}
-
-var winnerX = function() {
-
-  // determine if the winner is X
-
-  if (rowWinX() === 'win' || columnWinX() === 'win' || diagonalWinX() === 'win') {
-    return 'Winner is X';
-  }
-
-}
-
-var winnerO = function() {
-
-  //determine if the winner O
-  if (rowWinO() === 'win' || columnWinO() === 'win' || diagonalWinO() === 'win') {
-    return 'Winner is O';
-  }
-
-}
-
-var winnerDraw = function() {
-
-  // determine if the game is a draw
-  if (winnerX() !== 'Winner is X' && winnerO() !== 'Winner is O') {
-    console.log('The match was a draw!');
-  }
-
 }
 
 var getWinner = function() {
@@ -146,43 +110,38 @@ var getWinner = function() {
   var playerTwo = 'o';
 
   // find out which player wins the game
-  if (playerOne === 'x' && winnerX() === 'Winner is X') {
-    console.log('Player One Wins!');
+  if (rowWinX() === 'win' || columnWinX() === 'win' || diagonalWinX() === 'win' && playerOne === 'x') {
+
+    // update player One total score
+    playerOneTotal += 1;
+    $('#player-one-total').html(playerOneTotal);
+    screenDisplay.innerHTML = 'Player One Wins';
+
+    setTimeout(function() {
+      $('.square').empty();
+      screenDisplay.innerHTML = 'Play Again? Player One Move.';
+    }, 1000);
+    moves = 0;
+    token = 'o';
   }
 
-  else if (playerTwo === 'o' && winnerO() === 'Winner is O') {
-    console.log('Player Two Wins!');
-  }
+  else if (rowWinO() === 'win' || columnWinO() === 'win' || diagonalWinO() === 'win' && playerTwo === 'o') {
 
-  winnerDraw();
+    // update player Two total score
+    playerTwoTotal += 1;
+    $('#player-two-total').html(playerTwoTotal);
+    screenDisplay.innerHTML = 'Player One Wins';
+
+    setTimeout(function() {
+      $('.square').empty();
+      screenDisplay.innerHTML = 'Play Again? Player One Move.';
+    }, 1000);
+    moves = 0;
+    token = 'o';
+  }
+  
+  else {
+    return false;
+  }
 
 }
-
-
-
-
-
-
-
-//
-// var changeValueX = function() {
-//   if (playerOne === 'X') {
-//     document.getElementById('a').innerHTML = 'X';
-//   }
-// }
-//
-// var changeValueO = function() {
-//   if (playerTwo === 'O') {
-//     document.getElementById('c').innerHTML = 'O';
-//   }
-// }
-//
-// var changeA = document.getElementById('a');
-// changeA.addEventListener('click', changeValueX);
-
-//
-// var changeC = document.getElementById('c');
-// changeC.addEventListener('click', changeValueO);
-//
-// var changeE = document.getElementById('e');
-// changeE.addEventListener('click', changeValueX);
--- scripts/user_interaction.js
+++ scripts/user_interaction.js
@@ -1,46 +1,62 @@
+//declare the player token
+var token = 'o';
 
+// total moves played
+var moves = 0;
 
+// find the display
+var screenDisplay = document.getElementsByClassName('display')[0];
 
+// initial delay for the screen before it tells user what to do
+setTimeout(function() {
+  screenDisplay.innerHTML = 'It\'s Player One\'s Turn';
+}, 1000);
 
+var userDisplay = function() {
 
+  // changes display on top of the screen depending on user
+  if (token === 'o') {
 
+    // changes token to O and player Two's turn
+    screenDisplay.innerHTML = 'It\'s Player Two\'s Turn';
+    token = 'x';
+  }
 
+  else if (token === 'x') {
 
+    // changes token to O and player Two's turn
+    token = 'o';
+    screenDisplay.innerHTML = 'It\'s Player One\'s Turn'
+  }
+}
 
+// check for a tie
+var checkTie = function() {
 
+  // check made moves
+  moves += 1;
+  if (getWinner() === false) {
+    getWinner();
+    if (moves === 9) {
+      tieTotal += 1;
+      $('#tie-total').html(tieTotal);
+      screenDisplay.innerHTML = 'This Game is a Tie!';
 
+      setTimeout(function() {
+        $('.square').empty();
+        screenDisplay.innerHTML = 'Play Again? Player One Move.';
+      }, 1000);
+      moves = 0;
+      token = 'o';
+    }
+  }
+}
 
+$('tbody').on('click', '.square', function(event) {
 
+  // update the display, value in the square and check for a tie
+  userDisplay();
+  event.target.innerHTML = token;
+  checkTie();
 
-
-
-
-
-
-
-
-
-
-
-//
-// var changeValueX = function() {
-//   if (playerOne === 'X') {
-//     document.getElementById('a').innerHTML = 'X';
-//   }
-// }
-//
-// var changeValueO = function() {
-//   if (playerTwo === 'O') {
-//     document.getElementById('c').innerHTML = 'O';
-//   }
-// }
-//
-// var changeA = document.getElementById('a');
-// changeA.addEventListener('click', changeValueX);
-
-//
-// var changeC = document.getElementById('c');
-// changeC.addEventListener('click', changeValueO);
-//
-// var changeE = document.getElementById('e');
-// changeE.addEventListener('click', changeValueX);
+});
